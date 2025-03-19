0da3cf678216ad4b6e2071b1d84ec7779c400f55
Yamini <sharmayamini1999@gmail.com>
2025-03-19 22:17:10 +0530

    removed resize event listener

--- NavigationBar/index.js
+++ NavigationBar/index.js
@@ -1,14 +1,5 @@
 const menu = document.querySelector(".main-menu");
 
 function openMenu() {
-  menu.style.display = menu.style.display == "block" ? "none" : "block";
+  menu.classList.toggle("open");
 }
-
-window.addEventListener("resize", () => {
-  let currWidth = window.innerWidth;
-  if (currWidth > 500) {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-});
