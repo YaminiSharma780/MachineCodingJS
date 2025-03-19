3acadcac4cc5cc2c5d9a7c9944e58cce4b726ade
Yamini <sharmayamini1999@gmail.com>
2025-03-19 20:42:50 +0530

    working fine

--- NavigationBar/index.js
+++ NavigationBar/index.js
@@ -1,5 +1,14 @@
-const expander = document.querySelector(".expander");
-const header = document.querySelector("header");
-expander.addEventListener("click", () => {
-    header.style.display = header.style.display === "flex" ? "none" : "flex";
+const menu = document.querySelector(".main-menu");
+
+function openMenu() {
+  menu.style.display = menu.style.display == "block" ? "none" : "block";
+}
+
+window.addEventListener("resize", () => {
+  let currWidth = window.innerWidth;
+  if (currWidth > 500) {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
 });
