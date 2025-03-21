ce2f5a71463aa6f01c78017a5e63c4d56ed5b61b
Yamini <sharmayamini1999@gmail.com>
2025-03-21 10:36:14 +0530

    removed drag event in Tic-Tac-Toe

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -14,6 +14,9 @@ reset.addEventListener("click", () => {
   chance = true;
   allFilled = 0;
   chance = true;
+  squares.forEach((square) => {
+    square.style.pointerEvents = "auto";
+  });
   document.querySelector(".result").textContent = "Play to Win";
 });
 
@@ -21,23 +24,31 @@ gameContainer.addEventListener("click", (e) => {
   const currElement = e.target.dataset.index;
   console.log(currElement);
 
-  if (!unique[currElement]) {
-    if (chance) {
-      unique[currElement] = "X";
-      e.target.classList.add("fill-X");
-    } else {
-      unique[currElement] = "O";
-      e.target.classList.add("fill-O");
-    }
+  if (currElement) {
+    if (!unique[currElement]) {
+      if (chance) {
+        unique[currElement] = "X";
+        e.target.classList.add("fill-X");
+      } else {
+        unique[currElement] = "O";
+        e.target.classList.add("fill-O");
+      }
 
-    chance = !chance;
-    allFilled++;
+      chance = !chance;
+      allFilled++;
 
-    let result = checkWin();
-    if (allFilled == 9 || result.includes("Win")) {
-      document.querySelector(".result").textContent = result;
-      gameContainer.style.pointerEvents = "none";
+      let result = checkWin();
+      if (allFilled == 9 || result.includes("Win")) {
+        document.querySelector(".result").textContent = result;
+        gameContainer.style.pointerEvents = "none";
+        squares.forEach((square) => {
+          square.style.pointerEvents = "none";
+        });
+      }
     }
+  } else {
+    e.preventDefault();
+    console.log("dragging");
   }
 });
 
@@ -47,11 +58,22 @@ function checkWin() {
     let set = new Set();
     let player = "";
     for (let j = 0; j < 3; j++) {
+      // let key = ${i}-${j};: This creates a string key like "0-0", "0-1", "0-2", etc.,
+      // which corresponds to the unique object to retrieve the value of the square
+      // (either "X", "O", or undefined if unfilled)
       let key = `${i}-${j}`;
+
+      // checks which value is stored in square at key either "X", "O", or undefined
       set.add(unique[key]);
+
+      // player gets the value inserted in set for that key either "X" or "O"
       player = unique[key];
     }
+    console.log(set, player);
 
+    // if row/column has only single player then it's a win
+    // but if row/column is fully empty then undefined could win
+    // therefore we give condition to check if any player is there
     if (set.size == 1 && player) {
       return `Player ${player} Win`;
     }
@@ -66,7 +88,7 @@ function checkWin() {
       set.add(unique[key]);
       player = unique[key];
     }
-
+    console.log(set, player);
     if (set.size == 1 && player) {
       return `Player ${player} Win`;
     }
@@ -84,13 +106,14 @@ function checkWin() {
     i++;
     j++;
   }
-
+  console.log(set, player);
   if (set.size == 1 && player) {
     return `Player ${player} Win`;
   }
 
   // anti-diagonal
-  (i = 0), (j = 2);
+  i = 0;
+  j = 2;
   set.clear();
   while (i < 3 && j >= 0) {
     let key = `${i}-${j}`;
@@ -99,11 +122,10 @@ function checkWin() {
     i++;
     j--;
   }
-
+  console.log(set, player);
   if (set.size == 1 && player) {
     return `Player ${player} Win`;
   }
 
   return "Match draw";
 }
-
